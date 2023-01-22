96cdf170aafa03c5136e68b02f6714549d4313f3
Sören Kuklau <kuklau@trenz.de>
2023-01-22 14:14:56 +0100

    features/20-coalesce-notifications: #20 UI doesn't use it yet because cells need to be adjusted

--- Mastonaut/View Controllers/NotificationListViewController.swift
+++ Mastonaut/View Controllers/NotificationListViewController.swift
@@ -284,9 +284,9 @@ class NotificationListViewController: ListViewController<MastodonNotification>, 
 
 		filteredNotifications = filteredNotifications.filter { UserNotificationAgent.shouldShowNotification($0, accountNotificationPreferences: accountNotificationPreferences) }
 		
-		let coalescedNotifications = UserNotificationAgent.coalesceNotifications(filteredNotifications)
+//		let coalescedNotifications = UserNotificationAgent.coalesceNotifications(filteredNotifications)
 
-		super.prepareNewEntries(coalescedNotifications, for: insertion, pagination: pagination)
+		super.prepareNewEntries(filteredNotifications, for: insertion, pagination: pagination)
 	}
 
 	override func cellViewIdentifier(for notification: MastodonNotification) -> NSUserInterfaceItemIdentifier
