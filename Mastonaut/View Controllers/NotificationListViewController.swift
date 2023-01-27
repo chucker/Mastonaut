0a1a3a41351e59d387b5d7e816bdeaafece0cacb
Sören Kuklau <kuklau@trenz.de>
2023-01-27 22:27:09 +0100

    (formatting)

--- Mastonaut/View Controllers/NotificationListViewController.swift
+++ Mastonaut/View Controllers/NotificationListViewController.swift
@@ -188,11 +188,12 @@ class NotificationListViewController: ListViewController<MastodonNotification>, 
 	{
 		authorizedAccountProvider?.presentInSidebar(SidebarMode.tag(tag.name))
 	}
-	
-	func showStatusEdits(status: MastodonKit.Status, edits: [StatusEdit]) {
+
+	func showStatusEdits(status: MastodonKit.Status, edits: [StatusEdit])
+	{
 		authorizedAccountProvider?.presentInSidebar(SidebarMode.edits(status: status, edits: edits))
 	}
-	
+
 	func canDeleteOrEdit(status: Status) -> Bool
 	{
 		return currentUserIsAuthor(of: status)
@@ -224,7 +225,7 @@ class NotificationListViewController: ListViewController<MastodonNotification>, 
 	{
 		authorizedAccountProvider?.redraft(status: status)
 	}
-	
+
 	func edit(status: Status)
 	{
 		authorizedAccountProvider?.edit(status: status)
