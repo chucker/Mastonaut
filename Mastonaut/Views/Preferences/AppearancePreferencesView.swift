4f4b1139bc85ee4323ac40c3c95baac120a3ec34
Sören Kuklau <kuklau@trenz.de>
2023-02-09 21:39:15 +0100

    features/timeline-impact: add preferences UI

--- Mastonaut/Views/Preferences/AppearancePreferencesView.swift
+++ Mastonaut/Views/Preferences/AppearancePreferencesView.swift
@@ -12,14 +12,16 @@ import SwiftUI
 struct AppearancePreferencesView: View {
 	@AppStorage("appearance") var appearance: MastonautPreferences.Appearance = .auto
 
+	@AppStorage("areStatisticsEnabled") var areStatisticsEnabled: Bool = false
+
 	var preferences: MastonautPreferences?
 
 	let columns = [
 		GridItem(.fixed(260), alignment: .trailing),
 		GridItem(.fixed(380), alignment: .leading),
 	]
-	
-    var body: some View {
+
+	var body: some View {
 		LazyVGrid(columns: columns) {
 			Text("Appearance:")
 			Picker("", selection: $appearance) {
@@ -30,15 +32,31 @@ struct AppearancePreferencesView: View {
 			.pickerStyle(.radioGroup)
 			.horizontalRadioGroupLayout()
 			.padding(.horizontal, -8)
+
+			Text("Statistics:")
+				.padding(.top, 20)
+			Toggle("Collect statistics on peoples' toots", isOn: $areStatisticsEnabled)
+				.toggleStyle(.checkbox)
+				.padding(.top, 20)
+
+			Text("")
+			Text("Statistics data is always kept local. It comprises a toot's author and date, but not its actual contents.")
+				.font(.subheadline)
+				.foregroundColor(.secondary)
 		}
 		.onChange(of: appearance) { newValue in
 			Preferences.appearance = newValue
 		}
+		.onChange(of: areStatisticsEnabled) { newValue in
+			preferences?.areStatisticsEnabled = newValue
+		}
+		// AppKit layout hacks
+		.padding(.trailing, 15)
 	}
 }
 
 struct AppearancePreferencesView_Previews: PreviewProvider {
-    static var previews: some View {
+	static var previews: some View {
 		AppearancePreferencesView(appearance: .dark)
-    }
+	}
 }
